d37ed8446b797ce57410c3f846a5d0f8d8c034b1
EAMoiseenko <s1lverhron@gmail.com>
2021-11-30 16:00:17 +0300

    Code style

--- src/MarkdownAsset.php
+++ src/MarkdownAsset.php
@@ -9,13 +9,14 @@ use yii\web\AssetBundle;
 /**
  * @package s1lver\widgets
  */
-class MarkdownAsset extends AssetBundle {
-	public $sourcePath = '@vendor/s1lver/yii2-markdown-it/src/assets';
-	public $js = [
-		'js/markdown-it.js',
-		'js/markdown.js'
-	];
-	public $depends = [
-		AppAsset::class,
-	];
+class MarkdownAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/s1lver/yii2-markdown-it/src/assets';
+    public $js = [
+        'js/markdown-it.js',
+        'js/markdown.js'
+    ];
+    public $depends = [
+        AppAsset::class,
+    ];
 }
--- src/MarkdownIt.php
+++ src/MarkdownIt.php
@@ -9,40 +9,45 @@ use yii\widgets\InputWidget;
 /**
  * @package s1lver\widgets
  */
-class MarkdownIt extends InputWidget {
-	public array $buttons = [];
+class MarkdownIt extends InputWidget
+{
+    public array $buttons = [];
 
-	public function init(): void {
-		MarkdownAsset::register($this->view);
+    /**
+     * @inheritDoc
+     */
+    public function init(): void
+    {
+        MarkdownAsset::register($this->view);
 
-		echo Html::beginTag('div', ['class' => 'panel', 'id' => 'markdown-container']);
-		echo Html::beginTag('div', ['class' => 'panel-header']);
+        echo Html::beginTag('div', ['class' => 'panel', 'id' => 'markdown-container']);
+        echo Html::beginTag('div', ['class' => 'panel-header']);
 
-		$this->renderButton();
+        $this->renderButton();
 
-		echo Html::endTag('div');
-		echo Html::beginTag('div', ['class' => 'panel-content']);
-		echo Html::textarea('markdown-content', '', ['id' => 'markdown-content', 'class' => 'form-control', 'rows' => 5]);
-		echo Html::tag('section', '', ['id' => 'markdown-output']);
-		echo Html::endTag('div');
-		echo Html::endTag('div');
+        echo Html::endTag('div');
+        echo Html::beginTag('div', ['class' => 'panel-content']);
+        echo Html::textarea('markdown-content', '', ['id' => 'markdown-content', 'class' => 'form-control', 'rows' => 5]);
+        echo Html::tag('section', '', ['id' => 'markdown-output']);
+        echo Html::endTag('div');
+        echo Html::endTag('div');
 
-		parent::init();
-	}
+        parent::init();
+    }
 
-	private function renderButton(): void
-	{
-		if (!empty($this->buttons)) {
-			$id = 0;
-			foreach ($this->buttons as $button) {
-				$id ++;
-				$data = [
-					'data-button' => $id,
-					'data-before' => $button['buttonActions']['before']??'',
-					'data-after' => $button['buttonActions']['after']??'',
-				];
-				echo Html::button($button['label'], array_merge($data, $button['options']));
-			}
-		}
-	}
+    private function renderButton(): void
+    {
+        if (!empty($this->buttons)) {
+            $id = 0;
+            foreach ($this->buttons as $button) {
+                $id ++;
+                $data = [
+                    'data-button' => $id,
+                    'data-before' => $button['buttonActions']['before']??'',
+                    'data-after' => $button['buttonActions']['after']??'',
+                ];
+                echo Html::button($button['label'], array_merge($data, $button['options']));
+            }
+        }
+    }
 }
